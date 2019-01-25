4c23242141aa64b94356a1da137a7a4f7983c6e0
ziqiaozhou <ziqiao@cs.unc.edu>
2019-01-24 23:11:58 -0500

    scripts

---
+++ scripts/modelCount/cnf2count.sh
@@ -0,0 +1,8 @@
+file=$1
+out=$2
+cfile="concern.txt"
+jfile="jaccard.txt"
+#kleaver -out=$out.smt2 --print-smtlib $file
+#stp --output-CNF --disable-simplifications -r $out.smt2
+cp output_0.cnf ${out}.cnf
+python $KLEE_DIR/analyzer/SetInd.py ${out}.cnf name_cnf.txt $cfile $jfile
---
+++ scripts/modelCount/env.sh
@@ -0,0 +1,2 @@
+export KLEE_DIR=/playpen/ziqiao/thesis/codes/klee-all/klee
+export KLEAVER=$KLEE_DIR/klee_build*/bin/kleaver
